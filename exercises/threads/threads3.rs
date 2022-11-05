35a0fb15b860a504fe77ff900921d9f7af6c7ef6
wuzhengda <wuzhengda55@qq.com>
2022-11-05 23:45:16 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,7 +1,6 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -28,11 +27,11 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
-
+    let txc = tx.clone(); 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            txc.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -50,9 +49,8 @@ fn main() {
     let (tx, rx) = mpsc::channel();
     let queue = Queue::new();
     let queue_length = queue.length;
-
     send_tx(queue, tx);
-
+    
     let mut total_received: u32 = 0;
     for received in rx {
         println!("Got: {}", received);
